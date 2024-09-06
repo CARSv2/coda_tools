9a57ff0db920e55cbe85647ec8421ad4dddd572c
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-09-06 11:26:12 +1000

    setup init

---
+++ CARSv2_tools/__init__.py
@@ -0,0 +1,11 @@
+# CARSv2_tools/__init__.py
+
+# Make key functions and classes available at the package level
+#from .module1 import important_function
+#from .module2 import ImportantClass
+
+# Define package-wide constants
+VERSION = "0.0.1"
+
+# Optional: Initialize something at package import
+print(f"CARSv2_tools v{VERSION} initialized!")
