706365b87e8a1ebd1fa821967985b7662dc286af
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-09-09 16:13:06 +1000

    updates to incorporate yq querries of the yml file from shell script

--- scripts/rsync_CODA_HBA2Gadi.sh
+++ scripts/rsync_CODA_HBA2Gadi.sh
@@ -5,5 +5,13 @@
 # run this on tube-hba
 ############
 
-cd /oa-decadal-climate/work/observations/CARSv2_ancillary/CODA/CODAv1
-rsync -avP 201[0-9] tm4888@gadi.nci.org.au:/g/data/es60/users/thomas_moore/CODA/.
+# Extract the paths from the YAML file
+DIR_CODA=$(yq eval '.paths.path_tube_CODA' ../config.yml)
+DIR_logs=$(yq eval '.paths.path_tube_logs' ../config.yml)
+# Change to the directory & log
+cd "$DIR_CODA"
+echo "$(date): Changing directory to: $DIR_CODA" >> "$DIR_logs"/script_log.txt
+
+
+#rsync -avP {200[5-9],201[0-9],202[0-3]} tm4888@gadi.nci.org.au:/g/data/es60/users/thomas_moore/CODA/.
+.
