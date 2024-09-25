bdffecb09bd32add82c795fed8707c2fa20d7ff1
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-09-25 10:41:04 +1000

    #1 progress on YML and sh

--- scripts/rsync_CODA_HBA2Gadi.sh
+++ scripts/rsync_CODA_HBA2Gadi.sh
@@ -5,13 +5,17 @@
 # run this on tube-hba
 ############
 
+# move to DIR with shell script
+cd /home/moo270/code/CARSv2_tools/scripts
+
 # Extract the paths from the YAML file
 DIR_CODA=$(yq eval '.paths.path_tube_CODA' ../config.yml)
 DIR_logs=$(yq eval '.paths.path_tube_logs' ../config.yml)
+# Extract the search criteria from the YAML file
+glob_pattern=$(yq eval '.glob.rsync_pattern' ../config.yml)
 # Change to the directory & log
 cd "$DIR_CODA"
-echo "$(date): Changing directory to: $DIR_CODA" >> "$DIR_logs"/script_log.txt
-
-
-#rsync -avP {200[5-9],201[0-9],202[0-3]} tm4888@gadi.nci.org.au:/g/data/es60/users/thomas_moore/CODA/.
-.
+LOG_FILE_NAME="script_log_$(date +"%Y%m%d_%H%M%S").txt"
+echo "$(date): Changing directory to: $DIR_CODA" >> "$DIR_logs"/"$LOG_FILE_NAME"
+echo "rsync glob pattern:" "$glob_pattern" >> "$DIR_logs"/"$LOG_FILE_NAME"
+rsync -avP "$glob_pattern" tm4888@gadi.nci.org.au:/g/data/es60/users/thomas_moore/CODA/. >> "$DIR_logs"/"$LOG_FILE_NAME" 2>&1
