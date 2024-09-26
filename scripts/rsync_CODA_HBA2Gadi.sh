889f3d4471f36342049bb67590b33b7256fd0362
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-09-26 16:03:28 +1000

    fixed bugs and syntax for eval rsync with {} #1

--- scripts/rsync_CODA_HBA2Gadi.sh
+++ scripts/rsync_CODA_HBA2Gadi.sh
@@ -9,13 +9,17 @@
 cd /home/moo270/code/CARSv2_tools/scripts
 
 # Extract the paths from the YAML file
-DIR_CODA=$(yq eval '.paths.path_tube_CODA' ../config.yml)
+DIR_tube_CODA=$(yq eval '.paths.path_tube_CODA' ../config.yml)
+DIR_gadi_CODA=$(yq eval '.paths.path_gadi_CODA' ../config.yml)
 DIR_logs=$(yq eval '.paths.path_tube_logs' ../config.yml)
+
 # Extract the search criteria from the YAML file
 glob_pattern=$(yq eval '.glob.rsync_pattern' ../config.yml)
-# Change to the directory & log
-cd "$DIR_CODA"
+# setup log
 LOG_FILE_NAME="script_log_$(date +"%Y%m%d_%H%M%S").txt"
-echo "$(date): Changing directory to: $DIR_CODA" >> "$DIR_logs"/"$LOG_FILE_NAME"
-echo "rsync glob pattern:" "$glob_pattern" >> "$DIR_logs"/"$LOG_FILE_NAME"
-rsync -avP "$glob_pattern"/* tm4888@gadi.nci.org.au:/g/data/es60/users/thomas_moore/CODA/. >> "$DIR_logs"/"$LOG_FILE_NAME" 2>&1
+echo "DIR_tube_CODA:" $DIR_tube_CODA >> $DIR_logs/$LOG_FILE_NAME
+echo "DIR_gadi_CODA:" $DIR_gadi_CODA >> $DIR_logs/$LOG_FILE_NAME
+echo "DIR_logs:" $DIR_logs >> $DIR_logs/$LOG_FILE_NAME
+echo "rsync glob pattern:" $glob_pattern >> $DIR_logs/$LOG_FILE_NAME
+#eval rsync -avP --include */ --include *.nc --exclude * --exclude WOD2018*.nc "$DIR_tube_CODA/$glob_pattern" "tm4888@gadi.nci.org.au:$DIR_gadi_CODA/." >> "$DIR_logs/$LOG_FILE_NAME" 2>&1
+eval rsync -avP --include '*/' --exclude 'WOD2018*.nc' --include '*.nc'  --exclude '*' "$DIR_tube_CODA/$glob_pattern" "tm4888@gadi.nci.org.au:$DIR_gadi_CODA/." >> "$DIR_logs/$LOG_FILE_NAME" 2>&1
