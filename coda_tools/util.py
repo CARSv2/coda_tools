a448abe736dc9810f4d5fe8199c5b14cc05aa3dc
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-10-23 16:48:51 +1100

    test_function runs

---
+++ coda_tools/util.py
@@ -0,0 +1,36 @@
+"""
+util.py
+
+This module contains a collection of utility functions for coda_tools.
+Author = {"name": "Thomas Moore", "affiliation": "CSIRO", "email": "thomas.moore@csiro.au", "orcid": "0000-0003-3930-1946"}
+"""
+# Standard library imports
+import os
+import socket
+import yaml
+
+# Third-party imports
+#import numpy as np
+#from dask.distributed import Client, LocalCluster
+#from tabulate import tabulate
+#import xarray as xr
+#import intake
+
+
+# Local application imports (if needed)
+#from .my_local_module import my_function
+
+
+def test_function():
+    """
+    a test function.
+
+    Parameters:
+    None
+
+    Returns:
+    True
+    """
+    print("The coda_tools package is installed and working correctly....  ")
+    return True
+
